939381cf4208430e23b9a57c695a3786677b3dce
Yan <idy0013@gmail.com>
2014-08-11 22:14:28 +0800

    add sample tests

---
+++ test/test.js
@@ -0,0 +1,43 @@
+var fs = require('fs');
+var path = require('path');
+
+var Push = require('../lib/push');
+var Pull = require('../lib/pull');
+var concat = require('concat-stream');
+
+var tests = [];
+fs.readdirSync(__dirname).forEach(function(fil) {
+  var match = /^test-(\w+)\.xlsx$/.exec(fil);
+  if (match) {
+    tests.push(match[1]);
+  }
+});
+tests.forEach(function(test) {
+  var xlsxFile = path.join(__dirname, 'test-' + test + '.xlsx');
+  var jsonFile = path.join(__dirname, 'data-' + test + '.json');
+  var inputJson = require(jsonFile);
+  describe('Test case ' + test, function() {
+    var zipBuffer;
+    var push = new Push();
+    var pull = new Pull();
+    it('should push records', function (done) {
+      this.timeout(1500);
+      push.setXLSXStream(fs.createReadStream(xlsxFile));
+      push.records(inputJson);
+      push.pipe(concat(function(buf) {
+        zipBuffer = buf;
+        done();
+      }));
+    });
+    it('should pull the pushed value from zipBuffer', function (done) {
+      this.timeout(1500);
+      pull.setXLSXBuffer(zipBuffer);
+      pull.records(function(err, records) {
+        if (err) return done(err);
+        records.should.eql(inputJson);
+        done();
+      });
+    });
+  });
+});
+
